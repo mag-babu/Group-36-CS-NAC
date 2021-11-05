e25f017b10a62d74d8e46c1197e8778d14a6b471
svr1672 <60640798+svr1672@users.noreply.github.com>
2021-11-06 00:32:08 +0530

    Add files via upload

---
+++ consumer/rss_consumer.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from json import loads
+import json 
+from time import sleep
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer('articles',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group-id',
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="csproj",
+    database="news"
+)
+
+mycursor = mydb.cursor()
+
+for article in consumer:
+
+  try:
+      article_info = article.value
+      print(article_info)
+      info=article_info 
+      sql_str = "INSERT INTO news.articles (title, date_time,summary,topic,data_source,news_source) VALUES (%s, %s,%s,%s,%s,%s)"
+      values = (info['title'],info['published_date'],info['summary'],info['topic'], 'static',info['source_link'])
+      mycursor.execute(sql_str, values)
+      mydb.commit(); 
+      print('Information Saved Sucessfully ...')
+    
+  except Exception as  e :
+
+    print(f'Error while saving --> {e}')
+    
