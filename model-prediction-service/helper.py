533e24e4ec9e64614c9242242f203f8eae84e780
mag-babu <bmb.mag@gmail.com>
2021-11-10 01:11:38 +0530

    Update helper.py

--- model-prediction-service/helper.py
+++ model-prediction-service/helper.py
@@ -25,7 +25,7 @@ wnl = WordNetLemmatizer()
 def get_all(conn):
     # Get the park connection.
     spark = SparkSession.builder.config("spark.jars","jars/mysql-connector-java-8.0.25.jar").master("local").appName("PySpark_MySQL_test").getOrCreate()
-    df = spark.read.format("jdbc").option("url","jdbc:mysql://localhost:3306/news").option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "articles").option("user", "root").option("password", "csproj").load() 
+    df = spark.read.format("jdbc").option("url","jdbc:mysql://localhost:3306/news").option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "articles").option("user", "mag").option("password", "mag123").load() 
     print("Mysql Connected")
     print(df.show(3))
     return df
@@ -41,14 +41,14 @@ if __name__ == '__main__':
 # get total number of records
 def get_records_count(cursor):
     # execute the command
-    cursor.execute('''SELECT * FROM nac_news;''')
+    cursor.execute('''SELECT * FROM articles;''')
     return len(cursor.fetchall())
 
 
 # get by unq_serno
 def get_by_unq_serno(cursor, eid):
     # sql query
-    query = '''SELECT * FROM nac_news WHERE id = %s;'''
+    query = '''SELECT * FROM articles WHERE id = %s;'''
     # execute the command
     cursor.execute(query, [eid])
     return cursor.fetchone()
